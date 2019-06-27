ed27b55f40ee280b961e4bbb13236d36a87c576d
BartoszBilejczyk <bartosz.bilejczyk@10clouds.com>
2019-06-27 10:47:24 +0200

    styles

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,36 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/styles/partials/_mixins.scss";
+          @import "@/styles/partials/_variables.scss";
+          @import "@/styles/partials/_z-index.scss";
+        `
+      }
+    }
+  },
+  devServer: {
+    port: 8080
+  },
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  },
+  configureWebpack: {
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000
+      }
+    }
+  }
 };
